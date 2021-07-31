bb39d3f17cfaf2066cd83550cf8dd35d2f380ecc
jsegobia0 <jsegobia0@yahoo.com>
2021-07-31 10:54:12 -0700

    pseudo coded in script

---
+++ script.js
@@ -0,0 +1,11 @@
+// when opening the planner the cuurent day is displayed at the top of the page
+
+// when scrolling down the user is presented with time blocks for standard busines hours
+
+// when viewing time blocks for a specific day each time block is color-coded to indicate whether it is past, present, or future
+
+// when clicking on a time block the user can enter an event
+
+// when clicking the save button for "that" time block the text is saved in the local storage
+
+// when the page is refreshed the saved events persist
