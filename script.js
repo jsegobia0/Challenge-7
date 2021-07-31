b3aa77287008376eff221f8834d6fe137fb5ee82
jsegobia0 <jsegobia0@yahoo.com>
2021-07-31 11:36:22 -0700

    more pseudo code for script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // when opening the planner the cuurent day is displayed at the top of the page
 
+// vsr currentDay ;
+// .text current day to <p> ;
+// .append current day to <p> ;
+
 // when scrolling down the user is presented with time blocks for standard busines hours
+
+// create table rows for time-blocks ;
+// add table data for stadard business hours in table rows ;
 
 // when viewing time blocks for a specific day each time block is color-coded to indicate whether it is past, present, or future
 
+// var color for past ;
+// var color for present ;
+// var color for future ;
+// if past { color past} ;
+// if present { color present} ;
+// if future { color future} ;
+
+
 // when clicking on a time block the user can enter an event
 
+// on click = .text ;
+// .append .text entered ;
+
 // when clicking the save button for "that" time block the text is saved in the local storage
+
+// on click = save button ;
+// save to localStorage ;
 
 // when the page is refreshed the saved events persist
